81e1246205d981880d22d015e99009e044d4bbbe
0xDarkStar <111785550+0xDarkStar@users.noreply.github.com>
2023-05-25 19:09:44 -0400

    Update lvl4.py
    Changed the drawing method

--- lvls/lvl4.py
+++ lvls/lvl4.py
@@ -1,39 +1,54 @@
 import turtle
 from time import sleep
 from .lvlParts import *
+from .movement import movement
 
-def lvl4(user):
-    global userCopy, text, walls, wallDots
+def lvl4(user, pen):
+    global userCopy, walls
     walls = turtle.Turtle()
-    wallDots = []
+    walls.speed(0)
     border(walls)
 
-    wallDots = wall(-275, 75, 0, walls, 5, wallDots)
-    wallDots = wall(25, 75, 0, walls, 5, wallDots)
+    mazeMap = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ]
+    drawMap(mazeMap, walls)
 
-    wallDots = wall(-275, -75, 0, walls, 5, wallDots)
-    wallDots = wall(25, -75, 0, walls, 5, wallDots)
-
-    wallDots = wall(-275, 175, 0, walls, 2, wallDots)
-    wallDots = wall(-125, 175, 0, walls, 5, wallDots)
-    wallDots = wall(175, 175, 0, walls, 2, wallDots)
-
-    wallDots = wall(-275, -175, 0, walls, 2, wallDots)
-    wallDots = wall(-125, -175, 0, walls, 5, wallDots)
-    wallDots = wall(175, -175, 0, walls, 2, wallDots)
-
-    text = turtle.Turtle()
-    text.hideturtle()
-    text.penup()
-    text.speed(0)
-    text.goto(0, -250)
-    text.write("This is you", False, "Center", font = ("Arial", 12, "bold"))
-    text.goto(0, 250)
-    text.write("This is your copy", False, "Center", font = ("Arial", 12, "bold"))
-    text.goto(0, 25)
-    text.write("Your copy does the opposite of what you do", False, "Center", font = ("Arial", 12, "bold"))
-    text.goto(0, -25)
-    text.write("The goal in these levels is to land on your copy", False, "Center", font = ("Arial", 12, "bold"))
+    pen.color("black")
+    pen.hideturtle()
+    pen.penup()
+    pen.speed(0)
+    pen.goto(0, -250)
+    pen.write("This is you", False, "Center", font = ("Arial", 12, "bold"))
+    pen.goto(0, 250)
+    pen.write("This is your copy", False, "Center", font = ("Arial", 12, "bold"))
+    pen.goto(0, 25)
+    pen.write("Your copy does the opposite of what you do", False, "Center", font = ("Arial", 12, "bold"))
+    pen.goto(0, -25)
+    pen.write("The goal in these levels is to land on your copy", False, "Center", font = ("Arial", 12, "bold"))
+    pen.color("red")
 
     userCopy = turtle.Turtle()
     userCopy.penup()
@@ -53,59 +68,9 @@ def lvl4(user):
     user.speed(1)
     user.pendown()
     user.setheading(90)
+    reset = [(0, -250), (0, 250)]
+    collision = Collision(mazeMap, user, walls, userCopy)
+    menu = "/-------------\ \n| w = up      |\n| d = right   |\n| a = left    |\n| s = down    |\n| ~ = quit    |\n| r = reset   |\n|             |\n\-------------/\n"
 
-    return userCopy
+    movement(user, pen, menu, collision, 0, 0, reset, userCopy)
 
-def collision(turtles, pen, door): # Check if user is in a wall or out of bounds
-    xpos = turtles.xcor()
-    ypos = turtles.ycor()
-    pos = (xpos,ypos)
-    xposCopy = userCopy.xcor()
-    yposCopy = userCopy.ycor()
-    posCopy = (xposCopy,yposCopy)
-    if ypos <= -275 or ypos >= 275 or xpos <= -275 or xpos >= 275:
-        print("Don't try to leave my maze.", end = "\r")
-        sleep(1)
-        print("                                                            ", end = "\r")
-        turtles.back(25)
-    elif yposCopy <= -275 or yposCopy >= 275 or xposCopy <= -275 or xposCopy >= 275:
-        print("Don't try to get your copy to leave my maze.", end = "\r")
-        sleep(1)
-        print("                                                            ", end = "\r")
-        userCopy.back(25)
-    elif pos in wallDots and posCopy in wallDots:
-        print("Both of you hit a wall.", end = "\r")
-        sleep(1)
-        print("                                                            ", end = "\r")
-        turtles.back(25)
-        userCopy.back(25)
-    elif pos in wallDots:
-        print("You hit a wall.", end = "\r")
-        sleep(1)
-        print("                                                            ", end = "\r")
-        turtles.back(25)
-    elif posCopy in wallDots:
-        print("Your copy hit a wall.", end = "\r")
-        sleep(1)
-        print("                                                            ", end = "\r")
-        userCopy.back(25)
-    elif pos == posCopy: # Checks if they made it to the goal
-        Win = turtle.Turtle()
-        Win.hideturtle()
-        Win.goto(0,0)
-        Win.write("YOU WON", False, align="center", font = ("Arial", 40, "bold") ) # Turtle is spawned to write "YOU WON" in center of screen
-        sleep(2)
-        door = "lvl5"
-        pen.clear()
-        turtles.clear()
-        walls.clear()
-        text.clear()
-        userCopy.clear()
-        Win.clear()
-        turtles.penup()
-        turtles.hideturtle()
-        userCopy.penup()
-        userCopy.hideturtle()
-        return door
-    else:
-        return door
